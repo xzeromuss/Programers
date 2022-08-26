4f6f7df85ebc7d11e43b17e6161a0462c18dbb23
miguelangelcuervohernandez <miguelcuervo159@gmail.com>
2022-08-26 17:49:59 -0500

    Agregando Constructor, Getter y Setter de la clase Empresa

--- Ciclo03Programers/src/main/java/com/programers/Ciclo03Programers/model/Empresa.java
+++ Ciclo03Programers/src/main/java/com/programers/Ciclo03Programers/model/Empresa.java
@@ -8,4 +8,43 @@ public class Empresa {
     private String direccion;
     private String telefono;
     private String nitEmpresa;
+
+    public Empresa(String nombreEmpresa, String direccion, String telefono, String nitEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nitEmpresa = nitEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(String nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
 }
